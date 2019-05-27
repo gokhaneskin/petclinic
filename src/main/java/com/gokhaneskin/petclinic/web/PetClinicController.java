639814c4321559143d3194c6d4d00a5b0f5efdca
Gokhan Eskin <gokhaneskin34@gmail.com>
2019-05-27 18:16:55 +0300

    MVC Layer

---
+++ src/main/java/com/gokhaneskin/petclinic/web/PetClinicController.java
@@ -0,0 +1,29 @@
+package com.gokhaneskin.petclinic.web;
+
+import com.gokhaneskin.petclinic.service.PetClinicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PetClinicController {
+
+    @Autowired
+    private PetClinicService petClinicService;
+
+    @RequestMapping("/owners")
+    public ModelAndView getOwners(){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.addObject("owners",petClinicService.findOwners());
+        modelAndView.setViewName("owners");
+        return modelAndView;
+    }
+
+    @RequestMapping("/pcs")
+    @ResponseBody
+    public String welcome(){
+        return "Welcome to PetClinic World!";
+    }
+}
